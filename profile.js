c92b197220793ff28cd02c6fa5dd8becca06af61
jonboribbins <143540988+jonboribbins@users.noreply.github.com>
2023-09-27 13:13:49 -0400

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+        }
+        header h1 {
+            margin: 0;
+        }
+        .profile {
+            background-color: #fff;
+            padding: 20px;
+            margin: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        }
+        .profile img {
+            max-width: 200px;
+            border-radius: 50%;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>My Profile</h1>
+    </header>
+    <div class="profile">
+        <img src="profile-picture.jpg" alt="My Profile Picture">
+        <h2>John Doe</h2>
+        <p>Web Developer</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec quam at urna tincidunt vehicula.</p>
+        <a href="https://twitter.com/johndoe" target="_blank">Twitter</a>
+        <a href="https://linkedin.com/in/johndoe" target="_blank">LinkedIn</a>
+        <a href="https://github.com/johndoe" target="_blank">GitHub</a>
+    </div>
+</body>
+</html>
